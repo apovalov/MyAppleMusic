567a5f884c50b9575c0b4b034cef7d492921fe96
Valentin Sergeevich Shapovalov <apovalov@gmail.com>
2019-12-21 16:18:00 +0300

    SearchController was added

--- MyAppleMusic/SearchViewController.swift
+++ MyAppleMusic/SearchViewController.swift
@@ -8,10 +8,57 @@
 
 import UIKit
 
+
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "bad guy", artistName: "Vladimir Visocky"),
+                 TrackModel(trackName: "From The Inside", artistName: "Linkin Park")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        setupSearchBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        searchController.searchBar.delegate = self
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName) \n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "SonhImage")
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
