cadc4776a0287e2631bccff695aca33bf8524e55
Valentin Sergeevich Shapovalov <apovalov@gmail.com>
2019-12-21 17:42:41 +0300

    Pods & AF was added

--- MyAppleMusic/SearchViewController.swift
+++ MyAppleMusic/SearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 
 struct TrackModel {
@@ -60,5 +61,17 @@ class SearchViewController: UITableViewController {
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        AF.request(url).responseData{ dataResponse in
+            if let error = dataResponse.error {
+                print("Error received requestiong data: \(error.localizedDescription)")
+            }
+            guard let data = dataResponse.data else { return }
+            
+            let someString = String(data: data, encoding: .utf8)
+            print(someString ?? "")
+            
+            return
+        }
     }
 }
