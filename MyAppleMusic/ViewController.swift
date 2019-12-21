bf9d429c432567a0177e7ebc013ff091d82de366
Valentin Sergeevich Shapovalov <apovalov@gmail.com>
2019-12-21 13:30:18 +0300

    Initial Commit

---
+++ MyAppleMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppleMusic
+//
+//  Created by Macbook on 21.12.2019.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
